537658d0d13d223888c44ae7b70875b83c908465
Oleg Koval <kvl.olg@gmail.com>
2018-04-06 15:45:04 +0200

    init commit

---
+++ 01-binary-search/index.js
@@ -0,0 +1,12 @@
+// works with sorted arrays
+// begins by comparing the middle element of the array with the target value
+// if the target value matches the middle element, its position in the array is returned
+// If the target value is less than or greater than the middle element
+// the search continues in the lower or upper half of the array
+// respectively, eliminating the other half from consideration
+//
+
+
+const main() => {
+  
+}
